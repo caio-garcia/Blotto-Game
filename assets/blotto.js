dcea567f5417942d7d62b956f0d6519b5bdd5416
Caio Garcia <caiopassador@hotmail.com>
2022-06-13 15:05:01 +0100

    Added BlottoGame, PlayerColonel, NPCColonel classes with their respective constructors and methods

---
+++ assets/blotto.js
@@ -0,0 +1,107 @@
+class BlottoGame {
+  constructor() {
+    this.noOfTroops = 6;
+    this.noOfBattleFields = 3;
+    this.battleFields = [];
+  }
+  troopAssignment() {
+    return this.noOfTroops;
+  }
+
+  createBattleFields() {
+    for (let i = 0; i < this.noOfBattleFields; i++) {
+      this.battleFields.push(0);
+    }
+  }
+  raid(troops) {
+    for (let i = 0; i < this.battleFields.length; i++) {
+      this.battleFields[i] += troops[i];
+    }
+  }
+  winnerEvaluation() {
+    let playerScoring = 0;
+    let NPCScoring = 0;
+    for (let i = 0; i < this.battleFields.length; i++) {
+      if (this.battleFields[i] > 0) {
+        playerScoring += 1;
+      } else if (this.battleFields[i] < 0) {
+        NPCScoring += 1;
+      }
+    }
+
+    if (playerScoring > NPCScoring) {
+      return `Player Won`;
+    } else if (playerScoring < NPCScoring) {
+      return `NPC Won`;
+    } else {
+      return `This was a draw!`;
+    }
+  }
+}
+class PlayerColonel {
+  constructor(playerName) {
+    this.playerName = playerName;
+    this.noOfTroops = 6;
+  }
+  colonelName() {
+    return `Cel. ${this.playerName}`;
+  }
+  assignTroops(noOfTroops) {
+    this.noOfTroops = noOfTroops;
+  }
+}
+
+class NPCColonel {
+  constructor() {
+    this.NPCColonelName = "Cel. Hanke";
+    this.noOfTroops = 6;
+  }
+  colonelName() {
+    return this.NPCColonelName;
+  }
+  assignTroops(noOfTroops) {
+    this.noOfTroops = noOfTroops;
+  }
+}
+
+//TESTING
+
+//NAMES
+const myGame = new BlottoGame();
+
+const player1 = new PlayerColonel("Caio Garcia");
+
+console.log(player1.colonelName());
+
+const NPCPlayer = new NPCColonel();
+
+console.log(NPCPlayer.colonelName());
+
+//TROOPS
+myGame.noOfTroops = 18;
+
+console.log(myGame.noOfTroops);
+
+player1.assignTroops(myGame.noOfTroops);
+NPCPlayer.assignTroops(myGame.noOfTroops);
+
+console.log(player1.noOfTroops);
+console.log(NPCPlayer.noOfTroops);
+
+//BATTLE FIELDS
+
+myGame.createBattleFields();
+console.log(myGame.battleFields);
+
+//RAID
+plTroops = [2, 2, 2];
+npcTroops = [-1, -1, -4];
+
+myGame.raid(plTroops);
+myGame.raid(npcTroops);
+
+console.log(myGame.battleFields);
+
+//EVALUATION
+
+console.log(myGame.winnerEvaluation());
